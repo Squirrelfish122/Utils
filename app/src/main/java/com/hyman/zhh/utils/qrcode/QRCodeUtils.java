ae597840c05d03d141d5a8a9b4eb62f139577924
zhanghao <hyman.zhanghao@gmail.com>
2018-06-28 08:34:44 +0800

    添加二维码工具类

---
+++ app/src/main/java/com/hyman/zhh/utils/qrcode/QRCodeUtils.java
@@ -0,0 +1,200 @@
+package com.hyman.zhh.utils.qrcode;
+
+import android.graphics.Bitmap;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
+import com.google.zxing.qrcode.encoder.Encoder;
+import com.google.zxing.qrcode.encoder.QRCode;
+import com.hyman.zhh.utils.log.PrintLog;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class QRCodeUtils {
+
+    private static final String TAG = "QRCodeUtils";
+
+    private static HashMap<String, WeakReference<Bitmap>> mBitmaps;
+
+    private static final int QUIET_ZONE_SIZE = 4;
+
+    public static Bitmap generateQrCodeBitmap(String content, int qrCodeWidth, int qrCodeHeight) {
+        PrintLog.d(TAG, "content = " + content);
+        if (mBitmaps == null) {
+            mBitmaps = new HashMap<>();
+        } else {
+            WeakReference<Bitmap> weakReference = mBitmaps.get(content);
+            if (weakReference != null) {
+                Bitmap bitmap = weakReference.get();
+                if (bitmap != null) {
+                    PrintLog.d(TAG, "get bitmap from cache");
+                    return bitmap;
+                }
+            }
+        }
+
+        //定义二维码参数
+        Map hints = new HashMap();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");//设置编码
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);//设置容错等级
+        hints.put(EncodeHintType.MARGIN, 1);//设置边距,默认是5
+
+        try {
+            BitMatrix matrix = encode(content, BarcodeFormat.QR_CODE, qrCodeWidth, qrCodeHeight, hints);
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            // 二维矩阵转为一维像素数组,也就是一直横着排了
+            int[] pixels = new int[width * height];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    if (matrix.get(x, y)) {
+                        pixels[y * width + x] = 0xff000000;
+                    } else {
+                        pixels[y * width + x] = 0xffffffff;
+                    }
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            // 通过像素数组生成bitmap,具体参考api
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+            WeakReference<Bitmap> weakReference = new WeakReference<>(bitmap);
+            mBitmaps.put(content, weakReference);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static BitMatrix encode(String contents,
+                            BarcodeFormat format,
+                            int width,
+                            int height,
+                            Map<EncodeHintType,?> hints) throws WriterException {
+
+        if (contents.isEmpty()) {
+            throw new IllegalArgumentException("Found empty contents");
+        }
+
+        if (format != BarcodeFormat.QR_CODE) {
+            throw new IllegalArgumentException("Can only encode QR_CODE, but got " + format);
+        }
+
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Requested dimensions are too small: " + width + 'x' +
+                    height);
+        }
+
+        ErrorCorrectionLevel errorCorrectionLevel = ErrorCorrectionLevel.L;
+        int quietZone = QUIET_ZONE_SIZE;
+        if (hints != null) {
+            if (hints.containsKey(EncodeHintType.ERROR_CORRECTION)) {
+                errorCorrectionLevel = ErrorCorrectionLevel.valueOf(hints.get(EncodeHintType.ERROR_CORRECTION).toString());
+            }
+            if (hints.containsKey(EncodeHintType.MARGIN)) {
+                quietZone = Integer.parseInt(hints.get(EncodeHintType.MARGIN).toString());
+            }
+        }
+
+        QRCode code = Encoder.encode(contents, errorCorrectionLevel, hints);
+        return renderResult(code, width, height, quietZone);
+    }
+
+    /**
+     * 对 zxing 的 QRCodeWriter 进行扩展, 解决白边过多的问题
+     * <p/>
+     * 源码参考 {@link com.google.zxing.qrcode.QRCodeWriter#renderResult(QRCode, int, int, int)}
+     *
+     * @param code
+     * @param width
+     * @param height
+     * @param quietZone 取值 [0, 4]
+     * @return
+     */
+    private static BitMatrix renderResult(QRCode code, int width, int height, int quietZone) {
+        ByteMatrix input = code.getMatrix();
+        if (input == null) {
+            throw new IllegalStateException();
+        }
+
+        // xxx 二维码宽高相等, 即 qrWidth == qrHeight
+        int inputWidth = input.getWidth();
+        int inputHeight = input.getHeight();
+        int qrWidth = inputWidth + (quietZone * 2);
+        int qrHeight = inputHeight + (quietZone * 2);
+
+
+        // 白边过多时, 缩放
+        int minSize = Math.min(width, height);
+        int scale = calculateScale(qrWidth, minSize);
+        PrintLog.d("QRCodeUtils", "minSize = " + minSize + ", scale = " + scale);
+        if (scale > 0) {
+            int padding, tmpValue;
+            // 计算边框留白
+            padding = (minSize - qrWidth * scale) / QUIET_ZONE_SIZE * quietZone;
+            tmpValue = qrWidth * scale + padding;
+            PrintLog.d("QRCodeUtils", "padding = " + padding + ", tmpValue = " + tmpValue);
+            if (width == height) {
+                width = tmpValue;
+                height = tmpValue;
+            } else if (width > height) {
+                width = width * tmpValue / height;
+                height = tmpValue;
+            } else {
+                height = height * tmpValue / width;
+                width = tmpValue;
+            }
+        }
+
+        int outputWidth = Math.max(width, qrWidth);
+        int outputHeight = Math.max(height, qrHeight);
+
+        int multiple = Math.min(outputWidth / qrWidth, outputHeight / qrHeight);
+        int leftPadding = (outputWidth - (inputWidth * multiple)) / 2;
+        int topPadding = (outputHeight - (inputHeight * multiple)) / 2;
+
+        BitMatrix output = new BitMatrix(outputWidth, outputHeight);
+
+        for (int inputY = 0, outputY = topPadding; inputY < inputHeight; inputY++, outputY += multiple) {
+            // Write the contents of this row of the barcode
+            for (int inputX = 0, outputX = leftPadding; inputX < inputWidth; inputX++, outputX += multiple) {
+                if (input.get(inputX, inputY) == 1) {
+                    output.setRegion(outputX, outputY, multiple, multiple);
+                }
+            }
+        }
+
+        return output;
+    }
+
+
+    /**
+     * 如果留白超过5% , 则需要缩放
+     *
+     * @param qrCodeSize 二维码大小
+     * @param expectSize 期望输出大小
+     * @return 返回缩放比例, <= 0 则表示不缩放, 否则指定缩放参数
+     */
+    private static int calculateScale(int qrCodeSize, int expectSize) {
+        if (qrCodeSize >= expectSize) {
+            return 0;
+        }
+
+        int scale = expectSize / qrCodeSize;
+        int abs = expectSize - scale * qrCodeSize;
+        if (abs < expectSize * 0.05) {
+            return 0;
+        }
+
+        return scale;
+    }
+}
